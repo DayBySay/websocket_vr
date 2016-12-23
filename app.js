adb6d1f09df6b021bb4089d5afa9380d13e73b7a
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-12-23 18:26:45 +0900

    プレイヤーがログインした時の処理追加

--- app.js
+++ app.js
@@ -7,8 +7,35 @@ var server = http.createServer(function(req, res) {
 }).listen(3000);
 
 var io = socketio.listen(server);
+var player1, player2;
 
 io.sockets.on('connection', function(socket) {
+	socket.on("connected", function () {
+		if (player1 === undefined) {
+			player1 = socket.id;
+			io.to(socket.id).emit("set_player", "player1")
+			return;
+		}
+
+		if (player2 === undefined) {
+			player2 = socket.id;
+			io.to(socket.id).emit("set_player", "player2")
+			return;
+		}
+	});
+
+	socket.on("disconnect", function() {
+		if (socket.id === player1) {
+			player1 = undefined;
+			return;
+		}
+
+		if (socket.id == player2) {
+			player2 = undefined;
+			return;
+		}
+	});
+
     socket.on('move_box', function(data) {
         socket.broadcast.emit('move_box', data);
     });
