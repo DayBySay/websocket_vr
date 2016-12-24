7761baca07cdf619675bc5be616de39c02ba6f1f
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-12-24 16:33:36 +0900

    Herokuへのデプロイ準備

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
     res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).listen(3000);
+}).listen(process.env.PORT || 3000);
 
 var io = socketio.listen(server);
 var player1, player2;
