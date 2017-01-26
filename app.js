5635a9db4329530adf87be57c4b5d6d290bfe4d7
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-26 21:30:05 +0900

    peeridをserverに送るように

--- app.js
+++ app.js
@@ -7,19 +7,22 @@ var server = http.createServer(function(req, res) {
 }).listen(process.env.PORT || 3000);
 
 var io = socketio.listen(server);
-var player1, player2;
+var player1 = new Object;
+var player2 = new Object;
 
 io.sockets.on('connection', function(socket) {
-    socket.on("connected", function () {
+    socket.on("connected", function (peerid) {
         if (player1 === undefined) {
-            player1 = socket.id;
-            io.to(socket.id).emit("set_player", "player1")
+            player1.socketid = socket.id;
+			player1.peerid = peerid;
+            io.to(socket.id).emit("set_player", "player1");
             return;
         }
 
         if (player2 === undefined) {
-            player2 = socket.id;
-            io.to(socket.id).emit("set_player", "player2")
+            player2.socketid = socket.id;
+			player2.peerid = peerid;
+            io.to(socket.id).emit("set_player", "player2");
             return;
         }
     });
