ed59cfb53b2d6c59c6a633f0ece04f97a5607395
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-26 21:50:33 +0900

    ログアウトできなくなっていた

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var server = http.createServer(function(req, res) {
 }).listen(process.env.PORT || 3000);
 
 var io = socketio.listen(server);
-var player1 = new Object;
-var player2 = new Object;
+var player1, player2;
 
 io.sockets.on('connection', function(socket) {
     socket.on("connected", function (peerid) {
         if (player1 === undefined) {
+			player1 = {};
             player1.socketid = socket.id;
 			player1.peerid = peerid;
             io.to(socket.id).emit("set_player", "player1");
@@ -20,20 +20,22 @@ io.sockets.on('connection', function(socket) {
         }
 
         if (player2 === undefined) {
+			player2 = {};
             player2.socketid = socket.id;
 			player2.peerid = peerid;
             io.to(socket.id).emit("set_player", "player2");
+			io.to(socket.id).emit("call", player1.peerid);
             return;
         }
     });
 
     socket.on("disconnect", function() {
-        if (socket.id === player1) {
+        if (socket.id === player1.socketid) {
             player1 = undefined;
             return;
         }
 
-        if (socket.id == player2) {
+        if (socket.id == player2.socketid) {
             player2 = undefined;
             return;
         }
